a9016a05e26caea9d54a4ddd63cb1ffefa2fd64c
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-07 15:18:50 +0000

    added interface for DataProps

---
+++ src/types.d.ts
@@ -0,0 +1,37 @@
+interface APIProps {
+    /**
+     * The name of the product or business.
+     */
+    name: string
+    /**
+     * The URL link to a brand image to display to users.
+     */
+    logo: string
+    /**
+     * The URL to the Notion API being used
+     */
+    notionAPI: string
+}
+
+interface DataProps {
+    /**
+     * The type of category: 'Features' • 'Ideas' • 'Issues' • 'Other'.
+     */
+    category: string
+    /**
+     * A user's satisfaction rate with your product.
+     */
+    satisfactionRate: string
+    /**
+     * A user's feedback input.
+     */
+    feedback: string
+    /**
+     * A user's contact email. May be 'n/a' if they don't enter anything.
+     */
+    email: string
+    /**
+     * The date the feedback was provided by the user.
+     */
+    date: string
+}
