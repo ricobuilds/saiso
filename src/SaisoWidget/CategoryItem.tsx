19ba95d794c88b8cd4c01733c6b4c6b8e01d1feb
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-07 20:01:36 +0000

    styles and props for CItem

--- src/SaisoWidget/CategoryItem.tsx
+++ src/SaisoWidget/CategoryItem.tsx
@@ -6,11 +6,11 @@ interface Props extends HTMLAttributes<HTMLElement> {
     selected: boolean
 }
 
-export const CategoryItem = ({ icon, title, selected }: Props) => {
+export const CategoryItem = ({ icon, title, selected, onClick }: Props) => {
     return (
-        <div className="">
-            <span>{icon}</span>
-            <span>{title}</span>
+        <div className={`flex flex-col items-center justify-center w-20 h-16 group bg-sylver-100 border rounded select-none duration-200 ${selected ? 'opacity-100 border border-amethyst-500' : 'cursor-pointer hover:bg-amethyst-200 hover:border-amethyst-500'}`} onClick={onClick}>
+            <span className={`text-2xl duration-200 ${selected ? 'opacity-100' : 'opacity-40 group-hover:opacity-100'}`}>{icon}</span>
+            <span className={`text-m-copy sm:text-d-copy ${selected ? 'opacity-100' : 'opacity-40 group-hover:opacity-100'}`}>{title}</span>
         </div>
     )
 }
