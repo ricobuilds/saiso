aab9bbc94af3519fbe22fe23b032164c8cc58457
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-07 20:00:28 +0000

    added shell to CItem

--- src/SaisoWidget/CategoryItem.tsx
+++ src/SaisoWidget/CategoryItem.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { HTMLAttributes, ReactNode } from 'react'
 
-export const CategoryItem = () => {
+interface Props extends HTMLAttributes<HTMLElement> {
+    icon: ReactNode
+    title: string
+    selected: boolean
+}
+
+export const CategoryItem = ({ icon, title, selected }: Props) => {
     return (
-        <div className=""></div>
+        <div className="">
+            <span>{icon}</span>
+            <span>{title}</span>
+        </div>
     )
 }
