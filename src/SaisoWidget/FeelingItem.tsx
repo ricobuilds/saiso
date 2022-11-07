aa439e85dbb290083a817719673328782df3645e
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-07 19:55:35 +0000

    added styles and remaining props to FeelingItem

--- src/SaisoWidget/FeelingItem.tsx
+++ src/SaisoWidget/FeelingItem.tsx
@@ -1,14 +1,14 @@
 import React, { HTMLAttributes, ReactNode } from "react"
 
-interface Props extends HTMLAttributes<HTMLElement>{
+interface Props extends HTMLAttributes<HTMLElement> {
     emoji: ReactNode
     selected: boolean
 }
 
-export const FeelingItem = ({emoji, selected, onClick}: Props) => {
+export const FeelingItem = ({ emoji, selected, onClick }: Props) => {
     return (
-        <div className="">
-            <span>{emoji}</span>
-       </div> 
+        <div className={`flex items-center justify-center w-8 h-8 border rounded-full select-none duration-200 ${selected ? 'border-amethyst-500' : 'cursor-pointer hover:bg-amethyst-200 hover:border-amethyst-500'}`} onClick={onClick}>
+            <span className={`duration-300 text-2xl ${selected ? 'opacity-100' : 'opacity-40 hover hover:opacity-100'}`}>{emoji}</span>
+        </div>
     )
 }
